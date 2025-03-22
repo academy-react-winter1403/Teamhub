8ee68c087eef80648d6b5518d0cb89d9f87d73f3
abolfazl <abolfazl.yuni@gmail.com>
2025-03-22 13:23:24 +0330

    chiz-kardam
    ..............................

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,22 @@
-// import React from 'react'
+import { Fragment } from "react/jsx-runtime"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Fragment>
+      <div className="Header flex gap-70">
+        <div className="logo flex">
+          <img className="ml-2" src="./Group 33.png" alt="" />
+          هگزا اسکواد
+        </div>
+        <ul className="top-menu flex gap-12 font-light">
+          <li>دوره ها</li>
+          <li>اساتید</li>
+          <li>ارتباط با ما</li>
+          <li>اخبار مقالات</li>
+        </ul>
+      </div>
+
+    </Fragment>
   )
 }
 
--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,11 +1,17 @@
 import { Fragment } from 'react'
 import { Header } from "../Header/Header"
+import { Footer } from '../Footer/Footer'
 
 const Root = () => {
   return (
    <Fragment>
+
      <Header/>
+
      <div>Root</div>
+
+     <Footer/>
+     
    </Fragment>
   )
 }
