2ed08d71b07e7404fc73cd54c177eef1022a749e
abolfazl <abolfazl.yuni@gmail.com>
2025-03-24 22:27:02 +0330

    headerrrrrrrrr
    eeeeeeeeeeeee

--- src/components/Pages/Landing-Page/Landing.tsx
+++ src/components/Pages/Landing-Page/Landing.tsx
@@ -5,7 +5,7 @@ import BannerContentOne from "./LandnigTitles"
 const Landing = () => {
   return (
     <>
-      <div className="relative -top-8 bg-[url(/header-bg.svg)] bg-bottom bg-no-repeat bg-cover w-full h-[700px] min-h-screen mb-20 z-0 flex justify-center items-center">
+      <div className="relative -top-25 bg-[url(/header-bg.svg)] bg-bottom bg-no-repeat bg-cover w-full h-[900px] min-h-screen mb-20 z-0 flex justify-center items-center">
 
         <div className="flex flex-row justify-evenly w-full">
 
@@ -21,7 +21,7 @@ const Landing = () => {
                 </div> */}
 
           </div>
-          <div className="h-112 w-40 bg-[url(/codingLeft.svg)] bg-no-repeat bg-contain"></div>
+          <div className="h-112 w-40 bg-[url(/CodingLeft.svg)] bg-no-repeat bg-contain"></div>
 
         </div>
 
