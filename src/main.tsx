081bb533e16c44802236147bcd53f78bf9a7b815
callMeMmdzmn <77173590+Mohammadzamanhpv@users.noreply.github.com>
2025-03-25 21:28:16 +0330

    design Mock Data structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {MainRouter} from './config/MainRouter/MainRouter'
+import {MainRouter} from './config/router/MainRouter'
 
 
 createRoot(document.getElementById('root')!).render(
