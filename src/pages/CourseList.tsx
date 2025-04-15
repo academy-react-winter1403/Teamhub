d4f8bec9915fae4e76c48ee906e0b96be03f0a44
callMeMmdzmn <77173590+Mohammadzamanhpv@users.noreply.github.com>
2025-04-15 13:20:49 +0330

    Update courseList.tsx

--- src/pages/CourseList.tsx
+++ src/pages/CourseList.tsx
@@ -9,36 +9,37 @@ import CourseListTitles from "../components/courseList/CourseListTitles";
 import { CourseState } from "../core/constants/Types";
 import { PopularCoursesBtn } from "../components/courseList/PopularCoursesBtn";
 
-
 const CourseList = () => {
+  const [cardList, setCardList] = useState<CourseState[]>([]);
+  const [originalData, setoriginalData] = useState<CourseState[]>([]);
 
-
-  const [cardList, setCardList] = useState<CourseState[]>([])
-  const [originalData, setoriginalData] = useState<CourseState[]>([])
-
-  const getCourseData= async() => {
-    const response = await getCourseList()
-    const dataArray= Object.values(response)
-    setCardList(dataArray[0] as CourseState[])
-    setoriginalData(dataArray[0] as CourseState[])
-    console.log(dataArray[0])
-  }
+  const getCourseData = async () => {
+    const response = await getCourseList();
+    const dataArray = Object.values(response);
+    setCardList(dataArray[0] as CourseState[]);
+    setoriginalData(dataArray[0] as CourseState[]);
+    console.log(dataArray[0]);
+  };
 
   useEffect(() => {
-    getCourseData()
-  }, [])
+    getCourseData();
+  }, []);
 
   return (
     <div className="bg-[#FAFBFC] relative w-full flex flex-col justify-center items-center">
-      <CourseListTitles/>
+      <CourseListTitles />
 
       <div className="w-[90%] flex justify-center gap-5">
         <Filters />
 
         <div className="courses rounded-xl w-[70%]">
           <div className="tabs tabs-box flex justify-between bg-[#FAFBFC] mb-5">
-
-            <input type="radio" name="my_tabs_6" className="tab bg-[url(/grid-1.svg)] bg-no-repeat bg-center m-1 mb-8 " defaultChecked/>
+            <input
+              type="radio"
+              name="my_tabs_6"
+              className="tab w-10 rounded-full bg-[url(/grid-1.svg)] bg-no-repeat bg-center m-1 mb-8"
+              defaultChecked
+            />
 
             <div className="tab-content bg-[#FAFBFC]">
               <WrapperViewOne cardList={cardList} />
@@ -50,18 +51,12 @@ const CourseList = () => {
               className="tab w-10 rounded-full bg-[url(/grid-2.svg)] bg-no-repeat bg-center m-1 "
             />
             <div className="tab-content bg-[#FAFBFC] border-base-300">
-                <WrapperViewTwo
-                cardList={cardList}
-                />
+              <WrapperViewTwo cardList={cardList} />
             </div>
 
             <div className="flex w-[90%]">
-              <Search
-              setCardList={setCardList}
-              originalData={originalData}
-              />
-              <PopularCoursesBtn/>
-
+              <Search setCardList={setCardList} originalData={originalData} />
+              <PopularCoursesBtn />
             </div>
           </div>
           <Pagination />
